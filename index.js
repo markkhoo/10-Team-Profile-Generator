d1eb85acd39e0c745dd2fd6eb03c263af763d935
markeezy <markkhoo95@gmail.com>
2021-04-19 19:41:09 -0700

    flailing around

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require('inquirer');
 
 
-// TESTING!!!
+
+
+// TESTING
 const manager = new Manager();
+
+manager.getName();
+console.log(manager);
